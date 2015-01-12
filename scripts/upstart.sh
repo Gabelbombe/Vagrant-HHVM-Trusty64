e8e4cb21e09480f05db83b25aaf01cccec69e0c0
ehime <dodomeki@gmail.com>
2015-01-12 14:47:48 -0800

    Updating upstart processes

--- scripts/upstart.sh
+++ scripts/upstart.sh
@@ -3,47 +3,27 @@
 
 # CPR : Jd Daniel :: Ehime-ken
 # MOD : 2015-01-12 @ 14:28:11
-# VER : Version 1
+# VER : Version 2
 
 echo "==> Disabling Firewall..."
 sudo ufw disable
 
-if ! hash git 2>/dev/null; then
-  echo "==> Installing GIT Core..."
-  sudo apt-get install -y git
-  then
+echo "==> Updating..."
+sudo apt-get -y update
+sudo apt-get -y upgrade
 
-  echo "==> Updating..."
-  sudo apt-get -y update
-  sudo apt-get -y upgrade
+echo "==> Creating install dir...."
+ROOT='/tmp/HHVM-Install'
+mkdir $ROOT && cd $_
 
+## Get HipHop source code....
+echo "Building the HipHop Virtual Machine..."
+sudo apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0x5a16e7281be7a449
+# installs add-apt-repository
+sudo apt-get install software-properties-common
+sudo add-apt-repository 'deb http://dl.hhvm.com/ubuntu trusty main'
+sudo apt-get update
+sudo apt-get install -y hhvm
 
-  echo "Installing required packages..."
-  sudo apt-get install -y autoconf automake binutils-dev build-essential cmake g++ gawk git 	\
-  libboost-dev libboost-filesystem-dev libboost-program-options-dev libboost-regex-dev 		\
-  libboost-system-dev libboost-thread-dev libbz2-dev libc-client-dev libldap2-dev 			\
-  libc-client2007e-dev libcap-dev libcurl4-openssl-dev libdwarf-dev libelf-dev 				\
-  libexpat-dev libgd2-xpm-dev libgoogle-glog-dev libgoogle-perftools-dev libicu-dev 		\
-  libjemalloc-dev libmcrypt-dev libmemcached-dev libmysqlclient-dev libncurses-dev 			\
-  libonig-dev libpcre3-dev libreadline-dev libtbb-dev libtool libxml2-dev zlib1g-dev 		\
-  libevent-dev libmagickwand-dev libinotifytools0-dev libiconv-hook-dev libedit-dev 		\
-  libiberty-dev libxslt1-dev ocaml-native-compilers libsqlite3-dev libyaml-dev libgmp3-dev 	\
-  php5
-
-
-  echo "==> Creating install dir...."
-  ROOT='/tmp/HHVM-Install'
-  mkdir $ROOT && cd $_
-
-
-  ## Get HipHop source code....
-  echo "Building the HipHop Virtual Machine..."
-  git clone git://github.com/facebook/hhvm.git --depth=1 && cd hhvm
-  git submodule update --init --recursive
-  cmake .
-  make -j 4
-
-  sudo make install
-
-  echo "==> Reenabling Firewall"
-  sudo ufw enable
+echo "==> Reenabling Firewall"
+sudo ufw enable
