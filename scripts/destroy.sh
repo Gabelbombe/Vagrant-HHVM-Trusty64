67ab83b2d98f0f7fb3d2060f8fda8866bb7f07c4
ehime <dodomeki@gmail.com>
2015-01-12 17:15:43 -0800

    Destroys boxes

--- scripts/destroy.sh
+++ scripts/destroy.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
+# Destroys all Virtualbox environments (semi-gracefully....)
 
-VBoxManage list runningvms | awk '{print $2;}' | xargs -I vmid VBoxManage controlvm vmid savestate
-VBoxManage list runningvms | awk '{print $2;}' | xargs -I vmid VBoxManage controlvm vmid poweroff
+# CPR : Jd Daniel :: Ehime-ken
+# MOD : 2015-01-12 @ 17:15:15
+# VER : Version 1
+
+VBoxManage list runningvms |awk '{print $2;}' |xargs -I vmid VBoxManage controlvm vmid savestate
+VBoxManage list runningvms |awk '{print $2;}' |xargs -I vmid VBoxManage controlvm vmid poweroff
+
 vagrant global-status --prune
+
+while read LINE; do
+  [ -z "$LINE" ] && break;
+  vagrant destroy $(echo $LINE |awk '{print$1}')
+done < <(vagrant global-status |tail -n +3)
